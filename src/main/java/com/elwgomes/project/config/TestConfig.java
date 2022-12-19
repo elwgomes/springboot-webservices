d5ed5ede061149c393efeacf97936b082c50f8b5
Leonardo Gomes <elwgomes@icloud.com>
2022-12-19 16:08:11 -0300

    orderStatus enum

--- src/main/java/com/elwgomes/project/config/TestConfig.java
+++ src/main/java/com/elwgomes/project/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.elwgomes.project.config;
 
 import com.elwgomes.project.entities.Order;
 import com.elwgomes.project.entities.User;
+import com.elwgomes.project.enums.OrderStatus;
 import com.elwgomes.project.repositories.OrderRepository;
 import com.elwgomes.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,16 +43,19 @@ public class TestConfig implements CommandLineRunner {
         Order o1 = new Order(
                 null,
                 Instant.parse("2019-06-20T19:53:07Z"),
+                OrderStatus.PAID,
                 u1
         );
         Order o2 = new Order(
                 null,
                 Instant.parse("2019-07-21T03:42:10Z"),
+                OrderStatus.WAITING_PAYMENT,
                 u2
         );
         Order o3 = new Order(
                 null,
                 Instant.parse("2019-07-22T15:21:22Z"),
+                OrderStatus.DELIVERED,
                 u1
         );
 
