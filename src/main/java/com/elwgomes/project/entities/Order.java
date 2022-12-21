ec65355eabac48e1f89c1a4f7f3672aab1c6e014
Leonardo Gomes <elwgomes@icloud.com>
2022-12-21 10:11:10 -0300

    Payment, one-to-one association

--- src/main/java/com/elwgomes/project/entities/Order.java
+++ src/main/java/com/elwgomes/project/entities/Order.java
@@ -34,6 +34,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order () {
         //empty constructor
     }
@@ -43,6 +46,14 @@ public class Order implements Serializable {
         this.moment = moment;
         setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems () {
